dd7680f3123ac91ec39f47b04587aa65d5fc263c
Rob Bell <me@rob-bell.net>
2021-12-05 16:07:27 +0000

    Add bootloader which prints Hello, World

---
+++ boot.asm
@@ -0,0 +1,30 @@
+ORG 0x7c00
+BITS 16
+
+start:
+    mov si, message     ; Move address of message into si register
+    call print          ; Call print procedure
+    jmp $               ; Loop forever
+
+print:
+    mov bx, 0           ; Set page to 0
+.loop:
+    lodsb               ; Load character at address SI into AL and increment SI
+    cmp al, 0           ; Compare AL register to null terminator
+    je .done            ; Jump to done if comparison is true
+    call print_char     ; Call print_char procedure
+    jmp .loop           ; Jump to loop beginning
+.done:
+    ret                 ; Return from procedure
+
+print_char:
+    mov ah, 0eh         ; Set display character command for 0x10
+    int 0x10            ; Perform BIOS interrupt
+    ret                 ; Return from procedure
+
+message: db 'Hello, World!', 0
+
+times 510-($ - $$) db 0 ; Pad remaining bytes with zeroes
+dw 0xAA55               ; Add boot signature
+
+; List of interrupts http://www.ctyme.com/intr/rb-0106.htm
